a52eb2ab62da8d8f308c28481fe079e870778527
jainil8102000 <85861446+jainil8102000@users.noreply.github.com>
2024-10-25 06:05:38 +0000

    Add Basics

--- palindrome.js
+++
@@ -1,7 +0,0 @@
-function palindrome(string){
-    const strClean = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-    const reversedStr = strClean.split('').reverse().join('');
-    return reversedStr === strClean;
-}
-const testStr = "Tenet";
-console.log(palindrome(testStr));
