cdf1fd1704c5d1c93026fa11e96937426d6c630c
jainil8102000 <85861446+jainil8102000@users.noreply.github.com>
2024-10-25 06:57:24 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3+3) we are using nodeJS, not browser
+
+// Data Types
+// 1> Primitive DataTypes
+// 2> Non-Primitive DataTypes
+
+// 1> Primitive DataTypes
+    // number => 2 to power 53
+    // bigInt => ""
+    // String
+    // boolean => true/false
+    // null => Standalone value
+    // undefined => can be assigned as a value also
+    // symbol => unique
+
+// 1> Non-Primitive DataTypes
+    // object
+
+    console.log(typeof "Jainil"); // string
+    console.log(typeof null); // object
+    console.log(typeof undefined); // undefined
+    
+
+
+
+
