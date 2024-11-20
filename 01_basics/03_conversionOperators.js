d2f4f26d563909c534648cee698fe1da754e42e6
jainil8102000 <85861446+jainil8102000@users.noreply.github.com>
2024-11-20 06:22:25 +0000

    conversion operators

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,26 @@
+let score = "33abc"
+
+console.log(typeof(score)); // write as a method
+console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber); //output will be NaN beacuse there is text with number 
+
+//   NaN - Not a number
+//  "33" => 33
+//  "33abc" => NaN
+//  true => 1, false => 0
+ 
+let isLoggedIn = 1;
+
+let booleanIsloggedIn = Boolean(isLoggedIn);
+
+console.log(typeof booleanIsloggedIn);
+
+// 1 => true, 0 => false,
+// "" => false
+// "hitesh" => true
+
