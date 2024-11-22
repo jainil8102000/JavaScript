602c15f74a425f45864ee12d31b379e122d2a743
jainil8102000 <85861446+jainil8102000@users.noreply.github.com>
2024-11-22 06:01:47 +0000

    start string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,25 @@
+const name = "john"
+const repoCount = 50
+
+// console.log(`${name}  ${repoCount}`);
+
+
+const gameName = new String("Johhn Doe") 
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toLowerCase());
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('h'));
+
+
+const str = 'Mozilla';
+
+console.log(str.substring(1, 3));
+// Expected output: "oz"
+
+console.log(str.substring(2));
+// Expected output: "zilla"
+
--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -13,11 +13,9 @@ let userEmail;
 const id = Symbol('123');
 const anotherId = Symbol('123');
 
-console.log(id === anotherId); // false
+// console.log(id === anotherId); // false
 
 const bigNumber =  123453n
-
-
 
 
 // Reference Type (Non Primitive)
@@ -37,8 +35,39 @@ const myFunction = function(){
 
 // How to find datatypes
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 // Note: 
 
 // => Null's return value using typeof is "undefined"
+
+// ***************************************************************************
+
+
+// Memories
+// 1> Stack Memory (Uses is primitive datatypes)
+    // => We get copy of that variable
+
+// 2> Heap Memory (Uses is non-primitive datatypes)
+    // => We get reference of that variable
+
+let myName = "john doe";
+
+let anotherName = myName;
+anotherName = "gone"
+
+console.log(myName);
+console.log(anotherName);
+
+let user = {
+    name:"john",
+    email:"user@gmail.com"
+}
+
+let anotherUser = user
+
+anotherUser.email = "test@gmail.com"
+
+
+console.log(user.email);
+console.log(anotherUser.email);
