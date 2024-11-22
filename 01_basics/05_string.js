a62a4975908a7182a75abf9b0879c7257185a3e2
jainil8102000 <85861446+jainil8102000@users.noreply.github.com>
2024-11-22 06:46:49 +0000

    complelte string and methods

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -15,11 +15,37 @@ console.log(gameName.charAt(2));
 console.log(gameName.indexOf('h'));
 
 
+
+
 const str = 'Mozilla';
 
 console.log(str.substring(1, 3));
 // Expected output: "oz"
 
 console.log(str.substring(2));
+console.log(str.slice(-5, 2)); // ""
+console.log(str.slice(-5, -2)); // "zil"
 // Expected output: "zilla"
+
+const str2 = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(str2.slice(31));
+// Expected output: "the lazy dog."
+
+console.log(str2.slice(4, 19));
+// Expected output: "quick brown fox"
+
+console.log(str2.slice(-4));
+// Expected output: "dog."
+
+console.log(str2.slice(-9, -5));
+// Expected output: "lazy"
+
+
+const url = "https://google.com/jainil%20panchal"
+console.log(url.includes("jainil"));
+console.log(url.replace("%20", "-"));
+
+const splitString = "jainil-panchal"
+console.log(splitString.split("-"));
 
